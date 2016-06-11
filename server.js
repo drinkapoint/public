2251f14952fa8b353c983e4ebd824c76ab59c8c3
martinljones <martinleejones@outlook.com>
2016-06-11 14:40:24 -0500

    pushing everything local, changing organization name

--- server.js
+++ server.js
@@ -4,11 +4,15 @@
        cors = require('cors'),
        mongoose = require('mongoose'),
        port = process.env.PORT || 8181,
-       mongoUri = 'mongodb://localhost:27017/';
+       mongoUri = 'mongodb://localhost:27017/',
+       router = express.Router();
 
   app.use(bodyParser.json());
   app.use(cors());
   app.use(express.static(__dirname + '/www'));
+  app.use('/api', router);
+
+  //begin router.routes
 
   mongoose.connect(mongoUri);
   mongoose.connection.once('open', function() {
